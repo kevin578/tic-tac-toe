6325330018ed656e2a2370de110c058d7f3caa0f
kevin578 <kevinbriggs1@gmail.com>
2017-09-17 11:00:23 -0400

    minimax working

--- main.js
+++ main.js
@@ -1,74 +1,113 @@
 window.onload = function() {
 
-	/*  
-		board[0]    board[1]	board[2]
-		board[3]	board[4]	board[5]
-		board[6]	board[7]	board[8]
-	*/
+var mainBoard = [
+0,		1,		2,
+3,		4,		5,
+6,		7,		8];
 
-	var board = [
+var huPlayer = 'X'
+var aiPlayer = 'O'
 
-	"X",	"",		"", 
-	"O",	"",		"", 
-	"",		"",		"X"
+var turn = "human"
 
-	];
+function clicked() {
+	console.log("clicked")
+	/*if (turn = "human") {
+		document.getElementbyId(divName).innerHTML = huPlayer;
+	}*/
+}
 
-	var turn = {
-		player: 'X',
+function checkBoard(board, i) {
+  if( board[0] == i && board[1] == i && board[2] == i || //across top
+    board[3] == i && board[4] == i && board[5] == i || //across middle
+    board[6] == i && board[7] == i && board[8] == i || // across bottom
+    board[0] == i && board[3] == i && board[6] == i || // down left
+    board[1] == i && board[4] == i && board[7] == i || // center down
+    board[2] == i && board[5] == i && board[8] == i || // center right
+    board[0] == i && board[4] == i && board[8] == i || // diagonal top left to bottom right
+    board[6] == i && board[4] == i && board[2] == i // diagonal bottom left to top right
+    ) {
+    return true
+  }
+  else {
+    return false;
+  }
+}
+function getOpenSpots(arr) {
+  return arr.filter(function(x){
+    if (typeof x === 'number') {
+      return x;
+          }
+  });
+}
 
-		swap: function() {
-			this.player = this.player == 'X' ? 'O' : 'X';
-		}
-	} 
+function minimax(newBoard, player){
 
-	var moves = {};
+  
+  var openSpots = getOpenSpots(newBoard);
 
-	
-
-
-
-
-	
-
-
-
-
-
-
-	function checkBoard(i) {
-		if( board[0] == i && board[1] == i && board[2] == i ||
-			board[3] == i && board[4] == i && board[5] == i ||
-			board[6] == i && board[7] == i && board[8] == i ||
-			board[0] == i && board[3] == i && board[6] == i ||
-			board[1] == i && board[4] == i && board[7] == i ||
-			board[2] == i && board[5] == i && board[8] == i ||
-			board[4] == i && board[4] == i && board[8] == i ||
-			board[6] == i && board[4] == i && board[2] == i 
-			) {
-			return true
-		} 
-		else { 
-			return false;
-		}
+  
+  if (checkBoard(mainBoard, huPlayer)){
+     return {score:-1};
+  }
+	else if (checkBoard(mainBoard, aiPlayer)){
+    return {score:1};
 	}
+  else if (openSpots.length === 0){
+  	return {score:0};
+  }
 
-	function getOpenSpots(arr) {
-		var emptyArray = []
+    var moves = [];
 
-		for(var i = 0; i < arr.length; i++) {
-			if (arr[i].length < 1) {
-				emptyArray.push(i);
-			}
-		}
+    
+    for (var i = 0; i < openSpots.length; i++){
 
-		return emptyArray; 
-	} 
+		var move = {};    	
+    	move.index = newBoard[openSpots[i]];
+      	newBoard[openSpots[i]] = player;
+
+      	if (player == aiPlayer){
+        	var result = minimax(newBoard, huPlayer);
+        	move.score = result.score;
+      }
+      	else{
+        	var result = minimax(newBoard, aiPlayer);
+        	move.score = result.score;
+      }
+
+     	newBoard[openSpots[i]] = move.index;
+      	moves.push(move);
+    }
+
+	var bestMove;
+      
+    if(player === aiPlayer){
+
+        var bestScore = -8;
+        
+        for(var i = 0; i < moves.length; i++){
+          if(moves[i].score > bestScore){
+            bestScore = moves[i].score;
+            bestMove = i;
+          }
+        }
+      }
+      else {
+
+    var bestScore = 8;
+    for(var i = 0; i < moves.length; i++){
+      if(moves[i].score < bestScore){
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+
+  return moves[bestMove];
+}
 
 
-
-
-
+console.log (minimax(mainBoard, huPlayer))
 
 
 
